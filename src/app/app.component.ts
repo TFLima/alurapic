bdaedfb2b614efdb4a718f211ee3c5d5d8d588be
TN-Fagundes <thiago.fagundes@marinha.mil.br>
2023-09-18 09:43:56 -0300

    Módulo 2 finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'alurapic';
+  title = 'Alura Pic'
+
+  photos = [
+    {
+      url: "http://10.6.130.11:8081/assets/images/ccimar_0.png",
+      alt: "ccimar"
+    },
+    
+    {
+      url: "https://yt3.googleusercontent.com/axUhdWWrayKPlB7LO68ISweeWevXLAxOrXng2eefbtamO0r-jAbSX2AcFpubfhWILkOJL3r8=s900-c-k-c0x00ffffff-no-rj" ,
+      alt: "marinha"
+    }
+  ];
 }
