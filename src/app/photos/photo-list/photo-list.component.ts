5801966bcd56a6fe95fbb625a09a9a6d0230ee05
TN-Fagundes <thiago.fagundes@marinha.mil.br>
2023-11-29 08:14:50 -0300

    aula 6.10

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo.model';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -9,25 +12,44 @@ import { Photo } from '../photo/photo.model';
   styleUrls: ['./photo-list.component.css']
 })
 
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = false;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private photoService: PhotoService, private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private photoService: PhotoService) { }
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.paramMap.get('userName');
-
-    this.photoService
-      .listFromUser(userName ?? '')
-      .subscribe(photos => this.photos = photos)
+    this.userName = this.activatedRoute.snapshot.params['userName'];
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
   onKeyUp(event: any): void {
     if (event && event.target) {
-      this.filter = event.target.value;
+      this.debounce.next(event.target.value);
     }
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) 
+          this.hasMore = false;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
